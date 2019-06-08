546b9b4e8d648c41550d500652cbafdc2211137a
Lev Tikhonov <ltikhonov@tv-culture.ru>
2019-06-08 16:35:59 +0300

    working mandatory HW01 part

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,23 +1,34 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
+    private static String MEALS = "meals.jsp";
+    private List<MealTo> list;
+
     private static final Logger log = getLogger(MealServlet.class);
+
+    public MealServlet() {
+        super();
+        list = MealsUtil.hardcodedMealTo();
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to meals");
+        request.setAttribute("mealsWithExceed", list);
 
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        log.debug("redirect to meals");
+        request.getRequestDispatcher(MEALS).forward(request, response);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -7,5 +8,21 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+<c:if test="${pageContext.request.getAttribute(\"mealsWithExceed\") != null}">
+<table border=1>
+    <tr align="center">
+        <td>Название</td>
+        <td>Калории</td>
+        <td>Время</td>
+    </tr>
+
+    <c:forEach var="meal" items="${pageContext.request.getAttribute(\"mealsWithExceed\")}">
+    <tr bgcolor="${meal.exceed ? "red" : "green"}">
+        <td>${meal.description}</td>
+        <td>${meal.calories}</td>
+        <td>${meal.dateTime.toLocalDate()} ${meal.dateTime.toLocalTime()}</td>
+    </tr>
+    </c:forEach>
+    </c:if>
 </body>
 </html>
