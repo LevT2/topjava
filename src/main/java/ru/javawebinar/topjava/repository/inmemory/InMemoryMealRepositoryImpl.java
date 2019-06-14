69f04cd54d1446b273d2676e20ad9a414281073a
Lev Tikhonov <ltikhonov@tv-culture.ru>
2019-06-14 14:12:08 +0300

    introduced userId to MealRepository method signatures

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepositoryImpl.java
@@ -4,43 +4,95 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
-import java.util.Collection;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepositoryImpl implements MealRepository {
-    private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private Map<Key, Meal> repo = new ConcurrentHashMap<>();
     private AtomicInteger counter = new AtomicInteger(0);
 
+    class Key {
+        private int id;
+        private int userId;
+
+        public Key(int id, int userId) {
+            this.id = id;
+            this.userId = userId;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Key key = (Key) o;
+            return id == key.id &&
+                    userId == key.userId;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, userId);
+        }
+
+        @Override
+        public String toString() {
+            return "Key{" +
+                    "id=" + id +
+                    ", userId=" + userId +
+                    '}';
+        }
+    }
+
     {
-        MealsUtil.MEALS.forEach(this::save);
+        MealsUtil.MEALS.forEach(meal -> this.save(meal, 0));
     }
 
     @Override
-    public Meal save(Meal meal) {
+    public Meal save(Meal meal, int userId) {
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            repo.put(new Key(meal.getId(), userId), meal);
             return meal;
         }
         // treat case: update, but absent in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        Key first = getKey(meal.getId(), userId);
+        return repo.computeIfPresent(first, (key, oldMeal) -> meal);
     }
 
     @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    public boolean delete(int id, int userId) {
+        Key first = getKey(id, userId);
+        if (first == null) {
+            return false;
+        }
+        return repo.remove(first) != null;
     }
 
     @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    public Meal get(int id, int userId) {
+        Key first = getKey(id, userId);
+        if (first == null) {
+            return null;
+        }
+        return repo.get(first);
     }
 
     @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    public Collection<Meal> getAll(int userId) {
+        return repo.keySet().stream().
+                filter(key -> key.userId == userId).
+                map(key -> repo.get(key)).
+                sorted(Comparator.comparing(Meal::getDate).
+                        reversed()).
+                collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private Key getKey(int id, int userId) {
+        return repo.keySet().stream().
+                filter(key -> key.id == id && key.userId == userId).
+                findFirst().orElse(null);
     }
 }
 
