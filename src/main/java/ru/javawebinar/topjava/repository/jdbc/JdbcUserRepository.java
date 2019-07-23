24b3f514e4b557b480d03036351768449bfd9bf5
Lev Tikhonov <ltikhonov@tv-culture.ru>
2019-07-24 01:15:15 +0300

    refactored all but JdbcUserRepository#save

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -1,5 +1,7 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -9,14 +11,19 @@ import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Transactional(readOnly = true)
 @Repository
 public class JdbcUserRepository implements UserRepository {
+    protected final Logger log = LoggerFactory.getLogger(getClass());
 
     private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
 
@@ -52,6 +59,7 @@ public class JdbcUserRepository implements UserRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
         return jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
     }
@@ -59,18 +67,72 @@ public class JdbcUserRepository implements UserRepository {
     @Override
     public User get(int id) {
         List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
+        users.forEach(this::hydrateRoles);
         return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public User getByEmail(String email) {
-//        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
         List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
+        users.forEach(this::hydrateRoles);
         return DataAccessUtils.singleResult(users);
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        String query = "SELECT user_id, role, id , name, email, password, registered, enabled, calories_per_day " +
+                "FROM user_roles RIGHT JOIN users AS u on user_roles.user_id = u.id ORDER BY u.name, u.email";
+
+        return jdbcTemplate.query(query, rs -> {
+            Map<User, Set<Role>> map = new HashMap<>();
+
+            while (rs.next()) {
+                User user = new User(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("email"),
+                        rs.getString("password"),
+                        rs.getInt("calories_per_day"),
+                        rs.getBoolean("enabled"),
+                        rs.getDate("registered"),
+                        new HashSet<>()
+                );
+
+                var userRoles = map.computeIfAbsent(user, role -> new HashSet<>());
+                userRoles.add(Role.valueOf(rs.getString("role")));
+            }
+
+            return map.entrySet().stream().
+                    map(JdbcUserRepository::hydrateRoles).
+                    collect(Collectors.toList());
+        });
+    }
+
+
+    private static User hydrateRoles(Map.Entry<User, Set<Role>> userSetEntry) {
+        User bareUser = userSetEntry.getKey();
+        return new User(
+                bareUser.getId(),
+                bareUser.getName(),
+                bareUser.getEmail(),
+                bareUser.getPassword(),
+                bareUser.getCaloriesPerDay(),
+                bareUser.isEnabled(),
+                bareUser.getRegistered(),
+                userSetEntry.getValue()
+        );
+    }
+
+    private void hydrateRoles(User user) {
+        user.setRoles(getRoles(user.getId()));
+    }
+
+    private Collection<Role> getRoles(int userId) {
+        return jdbcTemplate.query("SELECT * FROM user_roles WHERE user_id = ?", JdbcUserRepository::mapUserRoles, userId);
+    }
+
+    private static Role mapUserRoles(ResultSet rs, int rowNum) throws SQLException {
+        String roleName = rs.getString("role");
+        return Role.valueOf(roleName);
     }
 }
